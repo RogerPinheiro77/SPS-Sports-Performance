8021d61572920d32696af625b88991cc7d54bd8f
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-17 07:37:52 +0100

    Agenda da atleta: vistas Diária/Semanal (defeito)/Mensal com grelha de pontinhos

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v6';
+const CACHE = 'sps-v7';
 const ASSETS = [
   './',
   './index.html',
