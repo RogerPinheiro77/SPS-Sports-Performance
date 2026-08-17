ee753baf63e7223f4003b9b8a16fa39421608439
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-17 15:43:17 +0100

    PSE & Wellness: expandir últimos 10 registos por atleta + relatório A4 de sessão (PSE do dia + Wellness do dia seguinte)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v9';
+const CACHE = 'sps-v10';
 const ASSETS = [
   './',
   './index.html',
