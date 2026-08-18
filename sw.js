a0f8103943d6a3c6702e65f58169cb0bb89659b5
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-18 10:58:03 +0100

    Reset password de atleta: link enviado já usa ?atleta=1

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v12';
+const CACHE = 'sps-v13';
 const ASSETS = [
   './',
   './index.html',
