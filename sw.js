b7c9f863204d0f6edf800261b68955a90ec3ec39
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-18 11:47:26 +0100

    Anamnese (Histórico Clínico) + PWA própria para Fisio e Nutri

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v14';
+const CACHE = 'sps-v15';
 const ASSETS = [
   './',
   './index.html',
