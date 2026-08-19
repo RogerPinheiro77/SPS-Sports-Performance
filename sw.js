3056b7b6f6f893c573aa242b5f10dad25ab0ce69
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-19 08:41:39 +0100

    sps-v25: separador Analise na Unidade de Treino - sessao (PSE/wellness/presencas/fisio) + cumulativo (ACWR ate a data)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v24';
+const CACHE = 'sps-v25';
 const ASSETS = [
   './',
   './index.html',
