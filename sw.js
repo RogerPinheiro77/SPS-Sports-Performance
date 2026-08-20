ea51cfb6d8b507d5fbf39fd2cf11c658d7404d1e
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-20 09:36:14 +0100

    sps-v33: Hora/Local de Concentração na Unidade de Treino + detalhe clicável na app da atleta

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v32';
+const CACHE = 'sps-v33';
 const ASSETS = [
   './',
   './index.html',
