fc6e393510638c16d888e146fd143e2eac2f232d
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-21 14:08:40 +0100

    sps-v45: scroll no Estado do Plantel (7 linhas) + coluna Visto da convocatória ativa

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v44';
+const CACHE = 'sps-v45';
 const ASSETS = [
   './',
   './index.html',
