5344a021bee41365ea2458b890d72b295d21fff0
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-21 13:37:29 +0100

    sps-v43: publicar convocatoria numa data/hora agendada (staff prepara com antecedencia, atleta so ve a partir dessa hora)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v42';
+const CACHE = 'sps-v43';
 const ASSETS = [
   './',
   './index.html',
