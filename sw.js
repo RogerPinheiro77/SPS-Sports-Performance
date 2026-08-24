f6da20277533ff2d54bc2ed29293b7d9bc3d6564
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-24 16:05:57 +0100

    sps-v59: Ficha Pré-Jogo (secretaria) - GR primeiro, resto por número de camisola (titulares e suplentes)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v58';
+const CACHE = 'sps-v59';
 const ASSETS = [
   './',
   './index.html',
