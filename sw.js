bb5acd204e09c68f930e5f31862074bab3b05cc6
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-24 13:36:51 +0100

    sps-v55: PDF da Condição do Plantel + PDF individual por quadro do Campo Tático

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v54';
+const CACHE = 'sps-v55';
 const ASSETS = [
   './',
   './index.html',
