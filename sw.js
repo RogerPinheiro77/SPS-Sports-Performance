73d45e9ee9ea1c4bc10533e357b10f2b1dc9ec9d
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-24 14:01:31 +0100

    sps-v56: Tático agora meio-campo em grelha como os outros 3 quadros (sem formações predefinidas, mantém nº+alcunha)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v55';
+const CACHE = 'sps-v56';
 const ASSETS = [
   './',
   './index.html',
