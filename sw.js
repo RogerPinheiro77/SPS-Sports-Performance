6dafbce9495b3da52ad73110bd68ddd7670227a4
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-25 14:41:06 +0100

    sps-v66: Ranking do Plantel (aba Jogos) + As Minhas Estatísticas na app da atleta

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v65';
+const CACHE = 'sps-v66';
 const ASSETS = [
   './',
   './index.html',
