ee65cbcaa230847245f6f3bc575cd06c86feae7b
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-25 14:51:00 +0100

    sps-v67: confirmação de presença (RSVP) na convocatória, staff + app da atleta

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v66';
+const CACHE = 'sps-v67';
 const ASSETS = [
   './',
   './index.html',
