cbf94b97cb2bc8645926b2b596c340d44054fb15
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-26 14:23:46 +0100

    sps-v77: análise automática do jogo no Relatório Completo

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v76';
+const CACHE = 'sps-v77';
 const ASSETS = [
   './',
   './index.html',
