09abbda7b4fb8bfb158ef12e64ab6400c2f84775
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-27 13:24:10 +0100

    sps-v84: permite editar nome de equipas + cria MFC W SUB/17

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v83';
+const CACHE = 'sps-v84';
 const ASSETS = [
   './',
   './index.html',
