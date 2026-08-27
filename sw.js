6695cdd5bfee5365ac8ffc6e19d45bc58abec12f
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-27 14:07:16 +0100

    sps-v85: convocatorias avulsas entre equipas (Treino/Jogo) + motivo Convocada

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v84';
+const CACHE = 'sps-v85';
 const ASSETS = [
   './',
   './index.html',
