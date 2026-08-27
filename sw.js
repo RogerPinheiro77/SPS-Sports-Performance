8fe05d5a6c164e4930678a999fa2cf683f1c690d
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-27 09:15:13 +0100

    sps-v82: registo de ações do adversário no SPS-Gameday (remates, cartões, cantos/livres defensivos por nº opcional)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v81';
+const CACHE = 'sps-v82';
 const ASSETS = [
   './',
   './index.html',
