36a589785b75a5026742410de8cfc493feffe89d
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-08-28 09:26:53 +0100

    sps-v91: renomear coluna UA Total para UA Extra na pagina Treinos Extra (clarificar que nao inclui Unidades de Treino)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v90';
+const CACHE = 'sps-v91';
 const ASSETS = [
   './',
   './index.html',
