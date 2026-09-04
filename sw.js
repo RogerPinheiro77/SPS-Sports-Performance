0e15183aa6688c9ed65b9d98af041cda07d78a4e
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-09-04 11:51:58 +0100

    sps-v131: seta de retrocesso na topbar Fisio/Nutri + avisos colapsáveis

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v130';
+const CACHE = 'sps-v131';
 const ASSETS = [
   './',
   './index.html',
