03b29a52fd5685dc6d79d245f022a619e0cf6732
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-09-04 15:56:50 +0100

    sps-v135: fix perda de confirmacoes RSVP - fusao campo-a-campo de convocatorias.rsvp/seenBy/djChecks no pushAppMeta

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v134';
+const CACHE = 'sps-v135';
 const ASSETS = [
   './',
   './index.html',
