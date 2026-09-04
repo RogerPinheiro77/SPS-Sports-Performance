19142f9ca9f2707ddc8ee2fbb63517a193c2fc07
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-09-04 12:38:28 +0100

    sps-v133: Gameday Home + Classificacao (Campeonato FPF + Taca) + Amistosos separados

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v132';
+const CACHE = 'sps-v133';
 const ASSETS = [
   './',
   './index.html',
