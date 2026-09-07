f17d99bcd3c8dfd00b86e6c2006ba2893ce81c5a
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-09-07 09:39:37 +0100

    sps-v137: novos ícones PWA - app Fisioterapia e app Gameday

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v136';
+const CACHE = 'sps-v137';
 const ASSETS = [
   './',
   './index.html',
