d43b7613f581ecce5493d2cdd39c233271dca524
Roger Pinheiro <c.roger.pinheiro@gmail.com>
2026-09-07 16:13:38 +0100

    sps-v146: botão Atualizar no topbar (repõe ícone Cloud partido, agora puxa dados frescos da cloud)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPS Sports Performance — Service Worker
 // Cache estratégico: app shell + CDN assets
 
-const CACHE = 'sps-v145';
+const CACHE = 'sps-v146';
 const ASSETS = [
   './',
   './index.html',
